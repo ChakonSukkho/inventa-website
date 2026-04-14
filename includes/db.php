45de97ede7900e7ed30b364e9d6250a192585a59
CHAKON A/L EH CHEH <chakon0927sukkho@gmail.com>
2026-04-14 13:01:51 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -2,24 +2,28 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-/* =========================
-   DATABASE CONFIG (LOCAL)
-========================= */
 $host = "inventa-server.mysql.database.azure.com";
-$user = "mlvpiarvob@inventa-server";
-$pass = "Inventa123";
-$db   = "inventa_db";
+$user = "mlvpiarvob";
+$pass = "Inventa123@";
+$db   = "inventa-database";
 $port = 3306;
 
-/* =========================
-   CONNECT DATABASE
-========================= */
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-/* =========================
-   CHECK CONNECTION
-========================= */
-if (!$conn) {
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (mysqli_connect_errno()) {
     die("DB ERROR: " . mysqli_connect_error());
 }
+
 ?>
