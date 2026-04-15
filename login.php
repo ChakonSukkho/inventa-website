a3a891b4f465b18827c2bd12cf2be73e49f87cf1
CHAKON A/L EH CHEH <chakon0927sukkho@gmail.com>
2026-04-15 17:23:03 +0800

    Update login.php

--- login.php
+++ login.php
@@ -31,7 +31,7 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Inventa | Student Talent Management System</title>
 <meta name="description" content="Inventa is a student talent management system that helps manage student profiles, achievements, talents, and certificates in one platform.">
-<meta name="robots" content="noindex, follow">
+<meta name="robots" content="index, follow">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <style>
