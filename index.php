9f958b81a72770d2e6d75032a5103b4b1e278ad7
CHAKON A/L EH CHEH <chakon0927sukkho@gmail.com>
2026-04-15 11:52:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-echo "INDEX WORKING";
+require_once "includes/init.php";
+
+if (!is_logged_in()) {
+    header("Location: login.php");
+    exit;
+}
+
+switch ($_SESSION['role']) {
+    case 'admin':
+    case 'staff':
+        header("Location: dashboard.php");
+        break;
+
+    case 'student':
+        header("Location: student_profile.php");
+        break;
+
+    default:
+        session_destroy();
+        header("Location: login.php");
+}
+
 exit;
