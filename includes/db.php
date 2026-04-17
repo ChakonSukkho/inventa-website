a47696b98a7fc8b6a86c8238c24d2c422df8c817
CHAKON A/L EH CHEH <chakon0927sukkho@gmail.com>
2026-04-17 12:04:26 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = "localhost";
+$host = "103.6.196.30";
 $user = "norfaiz_inventa_user";
 $pass = "1nvent@";
 $db   = "norfaiz_inventa_db";
